866b6bc485cec6b6203e8f8cf88498d328457e5b
Alec Drosu Laptop <drosualec@gmail.com>
2022-06-20 07:14:53 +0100

    add NODE_ENV

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,5 +1,8 @@
-const { JWT_SECRET = "secret" } = process.env;
+const user = require("../models/user");
+
+const { NODE_ENV, JWT_SECRET = "secret" } = process.env;
 
 module.exports = {
     JWT_SECRET,
+    NODE_ENV,
 };
