002c4e5aed8f773c200a9153b965f0b556c8ce47
Alec Drosu Laptop <drosualec@gmail.com>
2022-07-05 09:59:20 +0100

    change mongoose address back to original string

--- backend/app.js
+++ backend/app.js
@@ -18,10 +18,12 @@ app.use(cors());
 app.use(helmet());
 app.options("*", cors());
 
-const { CONNECTION_URL, PORT = 3000 } = process.env;
+// const { CONNECTION_URL, PORT = 3000 } = process.env;
 
-// mongoose.connect("mongodb://localhost:27017/aroundb");
-mongoose.connect(CONNECTION_URL);
+const { PORT = 3000 } = process.env;
+
+mongoose.connect("mongodb://localhost:27017/aroundb");
+// mongoose.connect(CONNECTION_URL);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
