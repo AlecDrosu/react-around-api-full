eef980935885f74ed3a64951a474dd5210273fbd
Alec Drosu <drosualec@gmail.com>
2022-08-29 16:49:36 -0400

    fix error logging

--- backend/middlewares/error-handler.js
+++ backend/middlewares/error-handler.js
@@ -1,4 +1,5 @@
 const errorHandler = (err, req, res, next) => {
+    console.error(err)
     const { statusCode = 500, message } = err;
     res.status(statusCode).send({ 
         message: statusCode === 500 ? 'Internal server error' : message
