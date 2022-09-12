b22f29417bd63f7f6ba151ba68e68aaf5ec5784f
Alec Drosu <drosualec@gmail.com>
2022-09-11 21:39:14 -0400

    fixed mongo errors

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-// const URLValidator = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i;
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
@@ -20,9 +19,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    // required: true,
     default: "https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg",
-    unique: false,
     validate: {
       validator: validator.isURL,
       message: "Invalid URL",
